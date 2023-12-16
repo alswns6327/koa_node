201c5bdbf88441b4e751bf5b903b4012573077fa
alswns6327 <alswns6327@naver.com>
2023-12-16 15:10:11 +0900

    children router setting

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,26 +1,12 @@
 const koa = require('koa');
 const Router = require('koa-router');
+const api = require('./api');
 
 const app = new koa();
 
 const router = new Router();
 
-router.get('/', (ctx) => {
-  ctx.body = 'Home';
-});
-
-router.get('/about/:name?', (ctx) => {
-  const { name } = ctx.params;
-  ctx.body = name ? `${name}의 소개` : 'About';
-});
-
-router.get('/post', (ctx) => {
-  const { id } = ctx.query;
-  const querystring = ctx.querystring;
-
-  console.log(querystring);
-  ctx.body = id ? `post #${id}` : '포스트 아이디가 없습니다.';
-});
+router.use('/api', api.routes());
 
 app.use(router.routes()).use(router.allowedMethods());
 
