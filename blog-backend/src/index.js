5b6b67620d31f3ab410c37fc0c405c3e66dd7592
alswns6327 <alswns6327@naver.com>
2023-12-16 14:02:52 +0900

    first commit

---
+++ blog-backend/src/index.js
@@ -0,0 +1,20 @@
+const koa = require('koa');
+const Router = require('koa-router');
+
+const app = new koa();
+
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.body = 'Home';
+});
+
+router.get('/about', (ctx) => {
+  ctx.body = 'About';
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+app.listen(4000, () => {
+  console.log('Listening to port 4000');
+});
