3b5134832a0c80d4efe77fc75e04c61b1c0aecab
alswns6327 <alswnsdl333@gmail.com>
2023-12-17 22:36:19 +0900

    mongoDB 쿼리 작성

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,3 +1,5 @@
+import Post from '../../models/post';
+
 let postId = 1; // id의 초기값
 
 const posts = [
@@ -10,102 +12,85 @@ const posts = [
 
 /* post write
 POST /api/posts
-{title, body}
+{
+  title: '제목',
+  body: '내용',
+  tags: ['태그1', '태그2']
+}
 */
-export const write = (ctx) => {
-  // REST API의 Request Body는 ctx.request.body에서 조회할 수 있습니다.
-  const { title, body } = ctx.request.body;
-  postId++;
-  const post = { id: postId, title, body };
-  posts.push(post);
-  ctx.status = 201;
-  ctx.body = post;
+export const write = async (ctx) => {
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /* Post Search
 GET /api/posts
 */
-export const list = (ctx) => {
-  ctx.body = posts;
+export const list = async (ctx) => {
+  try {
+    const posts = await Post.find().exec(); // exec() : 서버에 쿼리 요청을 하기 위해서 붙여줘야하는 함수
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
 
 /* 특정 포스트 조회
 GET /api/posts/:id
 */
-export const read = (ctx) => {
+export const read = async (ctx) => {
   const { id } = ctx.params;
-  // id 갑으로 글 조회
-  const post = posts.find((p) => p.id.toString() === id);
-
-  if (!post) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  ctx.body = post;
 };
 
 /* delete post
 DELETE /api/posts/:id
 */
-export const remove = (ctx) => {
+export const remove = async (ctx) => {
   const { id } = ctx.params;
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  console.log(id);
-  console.log(index);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  posts.splice(index, 1);
-  ctx.status = 204; // No Content 클라이언트에게 전송할 body가 없다.
-};
-
-/* update all columns of post
-PUT /api/posts/:id
-{title, body}
-*/
-export const replace = (ctx) => {
-  // PUT 메서드는 전체 포스트 정보를 입력하여 데이터를 통쨰로 교체할 때 사용
-  const { id } = ctx.params;
-  const index = posts.findIndex((p) => p.id.toString() === id);
-
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
-  }
-  (posts[index] = {
-    id,
-    ...ctx.request.body,
-  }),
-    (ctx.body = posts[index]);
 };
 
 /* update column of post
 PATCH /api/posts/:id
 {title?, body?}
 */
-export const update = (ctx) => {
+export const update = async (ctx) => {
   const { id } = ctx.params;
-  const index = posts.findIndex((p) => p.id.toString() === id);
-  if (index === -1) {
-    ctx.status = 404;
-    ctx.body = {
-      message: '포스트가 존재하지 않습니다.',
-    };
-    return;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 업데이트 된 값을 반환, false인 경우 업데이트 이전 값을 반환
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
   }
-  posts[index] = {
-    ...posts[index],
-    ...ctx.request.body,
-  };
-  ctx.body = posts[index];
 };
---
+++ blog-backend/src/models/post.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const PostSchema = new Schema({
+  title: String,
+  body: String,
+  tags: [String], // 문자열로 이루어진 배열
+  publishedData: {
+    type: Date,
+    default: Date.now, // 현재 날짜를 기본값으로 지정
+  },
+});
+
+const Post = mongoose.model('Post', PostSchema);
+// const Post = mongoose.model('Post', PostSchema, 'custom_book_collection'); 3번째 인자로 값을 주면 3번째 인자로 컬렉션 이름을 생성함
+export default Post;
