dc0ffa18dd617f0c8624aa454da58bcde7146137
alswns6327 <alswns6327@naver.com>
2024-03-13 19:34:53 +0900

    .

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -3,6 +3,9 @@ import koa from 'koa';
 import Router from 'koa-router';
 import bodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
+import serve from 'koa-static';
+import path from 'path';
+import send from 'koa-send';
 
 const { PORT, MONGO_URI } = process.env;
 
@@ -29,6 +32,19 @@ router.use('/api', api.routes());
 app.use(bodyParser());
 app.use(jwtMiddleware);
 app.use(router.routes()).use(router.allowedMethods());
+
+const buildDirectory = path.resolve(
+  __dirname,
+  '../../../react1/blog-frontend/build',
+);
+app.use(serve(buildDirectory));
+app.use(async (ctx) => {
+  // Not Found이고, 주소가 /api로 시작하지 않는 경우
+  if (ctx.status === 404 && ctx.path.indexOf('/api') !== 0) {
+    // index.html을 반환
+    await send(ctx, 'index.html', { root: buildDirectory });
+  }
+});
 
 const port = PORT || 4000;
 app.listen(port, () => {
